2206f5d4ce206485425e1fdce85611de22dc550a
Luke Savage <luke.savage92@gmail.com>
2015-06-30 13:15:44 -0500

    Implemented _.uniq(). Passes all tests.

--- src/underbar.js
+++ src/underbar.js
@@ -102,6 +102,15 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var uniquely = [];
+    
+    for(var i = 0; i < array.length; i++){
+      if(_.indexOf(uniquely, array[i]) === -1){
+        uniquely.push(array[i]);
+      }
+    }
+    
+    return uniquely;
   };
 
 
