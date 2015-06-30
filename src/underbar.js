eff894cc5038d7f7fab8836a307fb39374fd7128
Luke Savage <luke.savage92@gmail.com>
2015-06-30 12:57:22 -0500

    Implemented _.filter(). Passes all tests

--- src/underbar.js
+++ src/underbar.js
@@ -80,6 +80,15 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var results = [];
+    
+    _.each(collection, function(element){
+      if(test(element)){
+        results.push(element);
+      }
+    });
+    
+    return results;
   };
 
   // Return all elements of an array that don't pass a truth test.
